ff27ab65e45a581ed30fd1aee899984ee566bb25
Matthew Terrill <meterrill@gmail.com>
2020-09-11 14:13:28 -0700

    Minify HTML for production mode

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,17 @@ const rssPlugin = require('@11ty/eleventy-plugin-rss');
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
 
-module.exports = config => {
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy('./src/images/');
+// Transforms
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = config => {
+  // Only minify HTML if we are in production because it slows builds _right_ down
+  if (isProduction) {
+    config.addTransform('htmlmin', htmlMinTransform);
+  }
   // Add filters
   config.addFilter('dateFilter', dateFilter);
   config.addFilter('w3DateFilter', w3DateFilter);
